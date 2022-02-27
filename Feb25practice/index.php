31b21d79d628946937b5912f3932c553daf385fd
Egidijus999 <93944065+Egidijus999@users.noreply.github.com>
2022-02-27 22:55:20 +0200

    Add files via upload

---
+++ Feb25practice/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BAIKAL</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
+    <link rel="stylesheet" href="./dist/css/style.css">
+</head>
+<body>
+    <section>
+<div class="section1">
+    <div class="nav1">
+    <div class="logo1">
+    <h3 class="baikal">Baikal</h3>
+    <h5>FOUNDATION</h5>
+    </div>
+    <div class="nav2">        
+            <a href="#"> Homes</a><select ></select>
+            <a href="#"> About</a><select ></select>
+            <a href="#"> News</a><select ></select>
+            <a href="#"> Elements</a>
+            <button class="submit"><a href="#"> Purchase</a></button>        
+    </div>
+</div>
+    <div class="center">
+        <h4 class="title1">Success Stories</h4>
+        <h1 class="title2">START WITH YOU</h1>
+        <button class="submit1">LEARN MORE</button>
+    </div>
+</div>
+<div class="section2">
+<div class="text1">
+    <h2>A Bootstrap 4 website template for entrepreneurial community.        
+    </h2>
+    <div class="text2">Some of the coolest websites on earth are empowered by ThemeWagon. To continue the ambience, Baikal aims to help the rapidly growing entrepreneurial community to go online.It's loaded with many elements & features. See what you can do with it.
+    </div>
+    <p>For any questions please contact</p>
+    <a href="#">info@yourdomain.com</a>
+</div>
+</div>
+<div class="section3">
+    <div class="left1">
+        <div class="img"> <img src="./dist/img/baikal.jpg" alt=""> </div>
+        <h5 class="title3">ABOUT</h5>
+        <p class="text3">Find out about our organization, mission, our methods, and the results of our decades of advocacy.</p>
+    <a href="#">Learn More</a>
+    <i class="fa-solid fa-arrow-right-long"></i>
+    </div>
+    <div class="right1">
+        <div class="img"> <img src="./dist/img/baikal-2644653__340.jpg" alt=""> </div>
+        <h5 class="title3">PORTOFOLIO</h5>
+        <p class="text3">Check out some of our featured portfolio from many successful projects, carefully curated by us.</p>
+    <a href="#">See Portofolio</a>
+    <i class="fa-solid fa-arrow-right-long"></i>
+    </div>
+</div>
+<div class="section4">
+<li class="i11"><i class="fa-brands fa-twitter"></i></li>
+<li class="i1"><i class="fa-brands fa-instagram"></i></li>
+<li class="i1"><i class="fa-brands fa-youtube"></i></li>
+<li class="i1"><i class="fa-brands fa-pinterest"></i></li>
+</div>
+<div class="section5">
+<p>459 BROADWAY, NEW YORK(212) 555-0123PLACEHOLDER@GMAIL.COM</p>
+<p class="p1">Powered by</p>
+<a href="" class="href1">ThemeWagon</a>
+</div>
+    </section>    
+</body>
+</html>
